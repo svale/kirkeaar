03e5e073a9aa2e045c4dbd1f8bbc0865e99f67bc
svale <svale@feed.no>
2024-01-18 12:22:51 +0100

    Ad "Romjulssøndag" to list of readings

--- src/readings/readingsList.js
+++ src/readings/readingsList.js
@@ -123,6 +123,26 @@ const readingsList = new function() {
     ],
   }
 
+  this['Romjulssøndag'] = {
+    I: [
+      'Sal 1,1-6',
+      '1 Tim 3,16',
+      'Matt 2,13-15',
+    ],
+    II: [
+      '2 Mos 1,15-21',
+      'Apg 7,17-22',
+      'Luk 2,36-38',
+    ],
+    III: [
+      'Jes 66,10-13',
+      'Rom 11,33-36',
+      'Luk 2,25-35',
+    ],
+    F: [
+      'Luk 2,22-40',    ],
+  }
+
   this['Nyttårsaften'] = {
     I: [
       'Fork 3,1-2.4-7.11a',
